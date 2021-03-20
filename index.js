606374175128a96e533243fcfa134d54a5e0e77e
Justin <justerabyte@gmail.com>
2021-03-20 16:47:57 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Juster is the champion";
+
+document.body.append(newHeader);
